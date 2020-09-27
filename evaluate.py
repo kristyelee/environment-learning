e07e34e99e0dcd69e641d6ef47fb79e5002ba547
Kristy Lee <kristylee@berkeley.edu>
2020-09-27 16:17:12 -0700

    Test code

--- evaluate.py
+++ evaluate.py
@@ -6,13 +6,16 @@ import pretrain
 import model as our_model
 import baseline_model
 import dataset
+import tqdm
+import torch
 
 from absl import flags
 FLAGS = flags.FLAGS
 flags.DEFINE_bool('baseline', False, 'use baseline model')
 flags.DEFINE_bool('batch', False, 'use batch evaluation (only supported with some datasets)')
 flags.DEFINE_bool('batch_increasing', False, 'use batch evaluation with larger and larger data sizes')
-flags.DEFINE_string('correctness_log', None, 'file to write log indicating which predictions were correct')
+flags.DEFINE_string('correctness_log', 'dataset_sessions.txt', 'file to write log indicating which predictions were correct')
+
 
 def evaluate():
     total_correct = 0
@@ -25,7 +28,7 @@ def evaluate():
         session_examples = 0
         session_correct_list = []
 
-        for state, language, target_output in dataset.get_session_data(session_id):
+        for state, language, target_output in tqdm.tqdm(dataset.get_session_data(session_id)):
             predicted = model.predict(state, language)
 
             if predicted == target_output:
@@ -91,10 +94,13 @@ if __name__ == '__main__':
             print('No pretrained model found with prefix "%s"; running pretraining' % FLAGS.pretrain_prefix)
             pretrain.train()
         Model = our_model.Model
-
+    print("test")
     if FLAGS.batch_increasing:
+        print("1")
         evaluate_batch_increasing()
     elif FLAGS.batch:
+        print("2")
         evaluate_batch(100)
     else:
+        print("3")
         evaluate()
