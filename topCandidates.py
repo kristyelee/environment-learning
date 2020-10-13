4b1feb0d54a2fd901852b5b372482788868f2304
Kristy Lee <kristylee@berkeley.edu>
2020-10-12 23:07:15 -0700

    finished top k candidates code

--- topCandidates.py
+++ topCandidates.py
@@ -18,7 +18,7 @@ flags.DEFINE_bool('batch', False, 'use batch evaluation (only supported with som
 flags.DEFINE_bool('batch_increasing', False, 'use batch evaluation with larger and larger data sizes')
 flags.DEFINE_string('correctness_log', 'dataset_sessions.txt', 'file to write log indicating which predictions were correct')
 
-def topKCandidates(k, state, language, target_output, model):
+def topKCandidatesHelper(k, state, language, target_output, model):
     # Top K candidates for (state, language, target output)
     candidateTuples = []
     seenCandidates = set()
@@ -31,24 +31,17 @@ def topKCandidates(k, state, language, target_output, model):
             candidateTuples.append((discreteRepresentation, likelihood, predicted))
 
     candidateTuples.sort(reverse=True, key=lambda x: x[1])
-    candidates = []
-    #print()
 
     for i in range(k):
-        #print(candidateTuples[i][1])
-        candidates.append((candidateTuples[i][0], candidateTuples[i][2]))
+        if candidateTuples[i][2] == target_output:
+            return i
+
+    return float('inf')
 
 
-    for i in range(k):
-        if candidates[i][1] == target_output:
-            return True
-
-    return False
-
-
-def allTopKCandidates(k, n):
-    #sessions will have key-value pairs of session_id, session_data
-    sessions = dict()
+def topKCandidatesAccuracy(k, n):
+    # sessions will have key-value pairs of session_id, session_data
+    # sessions = dict()
 
     for session_id in dataset.get_session_ids():
         count = 0
@@ -57,28 +50,27 @@ def allTopKCandidates(k, n):
 
         for state, language, target_output in tqdm.tqdm(dataset.get_session_data(session_id)): 
             if count == n:
-                print("Top K accuracy: " + str(number_accurate / n))
-                return
+                break
 
             tuple_state = tuple([tuple(state[i]) for i in range(len(state))]) 
             tuple_target_output = tuple([tuple(target_output[i]) for i in range(len(target_output))])          
             tup = (tuple_state, language, tuple_target_output)
 
             # Add top K candidates list for this (state, language, target output) to session_data
-            k_candidate_success = topKCandidates(k, state, language, target_output, model)
+            k_candidate_success = stateTopKCandidates(k, state, language, target_output, model)
 
-            if k_candidate_success:
+            if k_candidate_success != float('inf'):
                 number_accurate += 1
 
             # Update model, as is done in evaluate() in evaluate.py
             model.update(state, language, target_output)
 
             count += 1
+        
+        print("Top K accuracy: " + str(number_accurate / count))
 
-    #     # Append all session_data to sessions
-    #     sessions[session_id] = number_accurate / n
-
-    # return sessions
+        with open("dataset_sessions_top_k_accuracies.txt", 'a') as f:
+            f.write(str(datetime.now()-start_time) + " " + str(session_id) + " " + str(number_accurate/count) + " \n")
 
 if __name__ == '__main__':
     print(sys.argv)
@@ -91,5 +83,4 @@ if __name__ == '__main__':
             print('No pretrained model found with prefix "%s"; running pretraining' % FLAGS.pretrain_prefix)
             pretrain.train()
         Model = our_model.Model
-    print("test")
-    print(allTopKCandidates(20))
+    topKCandidatesAccuracy(100, 300)
